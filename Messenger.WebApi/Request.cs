062685e8de26a748a6a2dd0c2492891318b869f0
Martynas Kan <martis347@gmail.com>
2015-06-24 22:25:54 +0300

    Added minimal console-based UI for trying out

---
+++ Messenger.WebApi/Request.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Messenger.WebApi
+{
+    public static class Request
+    {
+        public static string Username { set; private get; }
+        private static string RoomName { set; get; }
+
+        public static async Task<bool> Register()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:1234/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(String.Concat("api/users/createuser?username=", Username));
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static async Task<bool> JoinRoom(string roomName)
+        {
+            RoomName = roomName;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:1234/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(String.Concat("api/users/joinRoom?username=", Username, "&&roomName=", roomName));
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+
+            }
+        }
+
+        public static async Task<bool> CreateRoom(string roomName)
+        {
+            RoomName = roomName;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:1234/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(String.Concat("api/users/createRoom?username=", Username, "&&roomName=", roomName));
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static async Task<bool> Write(string message)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:1234/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(String.Concat("api/message/send?username=",Username,"&message=",message,"&roomname=",RoomName));
+
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
--- MessengerProject.Client/Program.cs
+++ MessengerProject.Client/Program.cs
@@ -1,9 +1,78 @@
-﻿namespace MessengerProject.Client
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Messenger.WebApi;
+
+namespace MessengerProject.Client
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Choose username: ");
+            Request.Username = Console.ReadLine();
+
+            Console.WriteLine("Commands list:\n" +
+                              "To register press R\n" +
+                              "To create room press C and type room name \n" +
+                              "To joinRoom press J and type room name \n" +
+                              "To type message to chat room press M and type your message\n" +
+                              "To EXIT press esc");
+            while (true)
+            {
+                ConsoleKeyInfo action = Console.ReadKey();
+
+                switch (action.KeyChar)
+                {
+                    case 'R':
+                        Console.WriteLine(Request.Register().Result
+                            ? "\bSuccesfully registered"
+                            : "\bRegistration failed");
+                        break;
+                    case 'r':
+                        Console.WriteLine(Request.Register().Result
+                            ? "\bSuccesfully registered"
+                            : "\bRegistration failed");
+                        break;
+                    case 'j':
+                        Console.WriteLine("\bEnter room name\n");
+                        Console.WriteLine(Request.JoinRoom(Console.ReadLine()).Result
+                            ? "Successfully joined room"
+                            : "Failed to join room");
+                        break;
+                    case 'J':
+                        Console.WriteLine("\bEnter room name\n");
+                        Console.WriteLine(Request.JoinRoom(Console.ReadLine()).Result
+                            ? "Successfully joined room"
+                            : "Failed to join room");
+                        break;
+                    case 'c':
+                        Console.WriteLine("\bEnter room name\n");
+                        Console.WriteLine(Request.CreateRoom(Console.ReadLine()).Result
+                            ? "Successfully created room"
+                            : "Failed to create room");
+                        break;
+                    case 'C':
+                        Console.WriteLine("\bEnter room name\n");
+                        Console.WriteLine(Request.CreateRoom(Console.ReadLine()).Result
+                            ? "Successfully created room"
+                            : "Failed to create room");
+                        break;
+                    case 'M':
+                        Console.WriteLine("\bEnter your message\n");
+                        Console.WriteLine(Request.Write(Console.ReadLine()).Result
+                            ? ""
+                            : "Failed to send message");
+                        break;
+                    case 'm':
+                        Console.WriteLine(Request.Write(Console.ReadLine()).Result
+                            ? "\b"
+                            : "\bFailed to send message");
+                        break;
+                    case (char)27:
+                        return;
+                }
+            }
         }
     }
 }
